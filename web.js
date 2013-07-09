ec9d950aea395d74f8d7475e488036cdc725a16d
Roman Pronskiy <root@pronskiy.com>
2013-07-09 12:36:25 +0000

    Added aoutput from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var html = 'index.html';
+  var buffer;
+  fs.readSync(html, buffer);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
